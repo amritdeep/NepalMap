4e6d7b337c3a57d8840864237512e5982aa45af7
AmritDeep <amritdeepdhungana@hotmail.com>
2014-07-30 20:52:39 +0545

    First Commit

---
+++ NepalMap/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  NepalMap
+//
+//  Created by innu thapa on 7/30/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
