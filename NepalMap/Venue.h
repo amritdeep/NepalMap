93af7a61553ac00fe0036c2392cd00e541ac705b
AmritDeep <amritdeepdhungana@hotmail.com>
2014-07-31 11:26:38 +0545

    Create Venue class and define name property

---
+++ NepalMap/Venue.h
@@ -0,0 +1,15 @@
+//
+//  Venue.h
+//  NepalMap
+//
+//  Created by innu thapa on 7/31/14.
+//  Copyright (c) 2014 innu thapa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Venue : NSObject
+
+@property (nonatomic, strong) NSString *name;
+
+@end
---
+++ NepalMap/Venue.m
@@ -0,0 +1,13 @@
+//
+//  Venue.m
+//  NepalMap
+//
+//  Created by innu thapa on 7/31/14.
+//  Copyright (c) 2014 innu thapa. All rights reserved.
+//
+
+#import "Venue.h"
+
+@implementation Venue
+
+@end
